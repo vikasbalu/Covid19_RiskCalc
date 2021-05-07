d5f70eb9adaf453a7ea71a60857dac22fec2d3fd
vikasbalu <60404890+vikasbalu@users.noreply.github.com>
2021-05-07 09:25:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    if output == 1:
+        msg=" High "
+    else :
+        msg=" Low "
+    
+    return render_template('index.html', prediction_text='The Risk Of Covid-19 for you is {} '.format(msg))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import pickle
+
+covid = pd.read_csv("data.csv")
+
+from sklearn.preprocessing import LabelEncoder
+e=LabelEncoder()
+
+covid['Breathing Problem']=e.fit_transform(covid['Breathing Problem'])
+covid['Fever']=e.fit_transform(covid['Fever'])
+covid['Dry Cough']=e.fit_transform(covid['Dry Cough'])
+covid['Sore throat']=e.fit_transform(covid['Sore throat'])
+covid['Running Nose']=e.fit_transform(covid['Running Nose'])
+covid['Asthma']=e.fit_transform(covid['Asthma'])
+covid['Chronic Lung Disease']=e.fit_transform(covid['Chronic Lung Disease'])
+covid['Headache']=e.fit_transform(covid['Headache'])
+covid['Heart Disease']=e.fit_transform(covid['Heart Disease'])
+covid['Diabetes']=e.fit_transform(covid['Diabetes'])
+covid['Hyper Tension']=e.fit_transform(covid['Hyper Tension'])
+covid['Abroad travel']=e.fit_transform(covid['Abroad travel'])
+covid['Contact with COVID Patient']=e.fit_transform(covid['Contact with COVID Patient'])
+covid['Attended Large Gathering']=e.fit_transform(covid['Attended Large Gathering'])
+covid['Visited Public Exposed Places']=e.fit_transform(covid['Visited Public Exposed Places'])
+covid['Family working in Public Exposed Places']=e.fit_transform(covid['Family working in Public Exposed Places'])
+covid['Wearing Masks']=e.fit_transform(covid['Wearing Masks'])
+covid['Sanitization from Market']=e.fit_transform(covid['Sanitization from Market'])
+covid['COVID-19']=e.fit_transform(covid['COVID-19'])
+covid['Dry Cough']=e.fit_transform(covid['Dry Cough'])
+covid['Sore throat']=e.fit_transform(covid['Sore throat'])
+covid['Gastrointestinal ']=e.fit_transform(covid['Gastrointestinal '])
+covid['Fatigue ']=e.fit_transform(covid['Fatigue '])
+
+
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.metrics import accuracy_score
+
+x=covid.drop('COVID-19',axis=1)
+y=covid['COVID-19']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.20)
+
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+model.fit(x_train, y_train)
+y_pred = model.predict(x_test)
+acc_logreg=model.score(x_test, y_test)*100
+acc_logreg
+
+from sklearn.ensemble import RandomForestRegressor
+model = RandomForestRegressor(n_estimators=1000)
+model.fit(x_train, y_train)
+acc_randomforest=model.score(x_test, y_test)*100
+acc_randomforest
+
+from sklearn.ensemble import GradientBoostingRegressor
+GBR = GradientBoostingRegressor(n_estimators=100, max_depth=4)
+GBR.fit(x_train, y_train)
+acc_gbk=GBR.score(x_test, y_test)*100
+acc_gbk
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=20)
+knn.fit(x_train, y_train)
+y_pred = knn.predict(x_test)
+acc_knn=knn.score(x_test, y_test)*100
+acc_knn
+
+from sklearn import tree
+t = tree.DecisionTreeClassifier()
+t.fit(x_train,y_train)
+y_pred = t.predict(x_test)
+acc_decisiontree=t.score(x_test, y_test)*100
+acc_decisiontree
+
+from sklearn.naive_bayes import GaussianNB
+model = GaussianNB()
+model.fit(x_train,y_train)
+acc_gaussian= model.score(x_test, y_test)*100
+acc_gaussian
+
+from sklearn import svm
+clf = svm.SVC(kernel='linear')
+clf.fit(x_train, y_train)
+y_pred = clf.predict(x_test)
+acc_svc=clf.score(x_test, y_test)*100
+acc_svc
+
+dtc=t.fit(x,y)
+
+pickle.dump(dtc, open('model.pkl','wb'))
+
+model = pickle.load(open('model.pkl','rb'))
